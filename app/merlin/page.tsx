0a7e67174568dce0c74e28824456d9b5b227c0ec
Vikas Malhotra <malhotra.vikas@gmail.com>
2025-03-05 15:27:26 -0500

    No BotPress TSX version

--- app/merlin/page.tsx
+++ app/merlin/page.tsx
@@ -2,14 +2,12 @@
 //import MerlinChat from "@/components/merlin";
 
 // This is mixed AI solution
-import Merlin from "@/components/merlin-botpress"
 
 
 export default function MerlinBotPage() {
   return (
     <main className="min-h-screen bg-background p-4">
       <div className="container mx-auto space-y-8">
-        <Merlin />
       </div>
     </main>
   )
